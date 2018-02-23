5bc31126923259d50a897ba25934746661936331
安睿 <382867778@qq.com>
2018-02-23 14:14:53 +0800

    在提交一次

--- src/app/directive/drag-drop/drop.directive.ts
+++ src/app/directive/drag-drop/drop.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[app-droppable]'
+  selector: '[app-droppable][dropCalss]'
 })
 export class DropDirective {
 
-  constructor() { }
+  @Input() dropCalss:string;
+
+  constructor(private el: ElementRef, private rd: Renderer2) { }
+
+
+  @HostListener('dragenter', ['$event'])
+  onDragEnter(ev: Event) {
+    if (this.el.nativeElement == ev.target) {
+      this.rd.addClass(this.el.nativeElement, this.dropCalss)
+    }
+  }
+  @HostListener('dragover', ['$event'])
+  onDragOver(ev: Event) {
+    if (this.el.nativeElement == ev.target) {
+      this.rd.addClass(this.el.nativeElement, this.dropCalss)
+    }
+  }
+  @HostListener('dragleave', ['$event'])
+  onDragLeave(ev: Event) {
+    if (this.el.nativeElement == ev.target) {
+      this.rd.removeClass(this.el.nativeElement, this.dropCalss)
+    }
+  }
+  @HostListener('drop', ['$event'])
+  onDrop(ev: Event) {
+    if (this.el.nativeElement == ev.target) {
+      this.rd.removeClass(this.el.nativeElement, this.dropCalss)
+    }
+  }
 
 }
